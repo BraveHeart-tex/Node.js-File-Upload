55fa108e5b527af2e6e2fef1969faf4931636b3a
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2022-04-14 12:07:28 +0300

    ilk commit

---
+++ controllers/uploadsController.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: 'file-upload',
+    }
+  );
+  fs.unlinkSync(req.files.image.tempFilePath);
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
+};
+
+// const uploadProductImageLocal = async (req, res) => {
+//   // check if img file exists
+//   if (!req.files) {
+//     throw new CustomError.BadRequestError('No file uploaded');
+//   }
+
+//   const productImage = req.files.image;
+
+//   // check format
+//   if (!productImage.mimetype.startsWith('image')) {
+//     throw new CustomError.BadRequestError('Please upload an image');
+//   }
+
+//   // check size
+//   const maxSize = 1024 * 1024; // 1 MB
+//   if (productImage.size > maxSize) {
+//     throw new CustomError.BadRequestError(
+//       'Please upload an image smaller than 1MB'
+//     );
+//   }
+
+//   const imagePath = path.join(
+//     __dirname,
+//     '../public/uploads/' + `${productImage.name}`
+//   );
+//   await productImage.mv(imagePath);
+//   return res
+//     .status(StatusCodes.OK)
+//     .json({ image: { src: `/uploads/${productImage.name}` } });
+// };
+
+module.exports = {
+  uploadProductImage,
+};
